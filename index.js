de5c9186133400e0afe7504bba6e277dae526aa4
Shadow Monarch <shadow-monarch@shadow-monarch.Shadow>
2025-12-17 10:00:48 +0600

    Deployed to vercel

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
         // Send a ping to confirm a successful connection
         const db = client.db('tutor-owl');
         const userCollection = db.collection('users');
@@ -979,8 +979,8 @@ async function run() {
 
 
 
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        // await client.db("admin").command({ ping: 1 });
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
